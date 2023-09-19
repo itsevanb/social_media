2657fd09d1c793b0e8221c00fb9b7091c883cb52
Evan <116227653+itsevanb@users.noreply.github.com>
2023-09-19 14:48:18 -0500

    Right side bar done, for now..

--- constants/index.js
+++ constants/index.js
@@ -22,7 +22,7 @@ export const sidebarLinks = [
     {
       imgURL: "/assets/community.svg",
       route: "/communities",
-      label: "Communitites",
+      label: "Networks",
     },
     {
       imgURL: "/assets/user.svg",
