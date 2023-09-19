c0e60642cd7dca24249f121b2023425fe3ca432f
Evan <116227653+itsevanb@users.noreply.github.com>
2023-09-18 23:28:09 -0500

    Auth is created
    singing in, and up is now done, thanks to clerk. pretty cool third party library to work with/ explore

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,22 +1,26 @@
-import { ClerkProvider } from "@clerk/nextjs"
-import { Inter } from "next/font/google"
 
 import '../globals.css'
+import { ClerkProvider } from "@clerk/nextjs"
+import { Inter } from "next/font/google"
 
 export const metadata = {
     title: 'social-media',
     description: 'A next.js 13 Meta Threads Application Clone, inspired by Javascript Mastery on Youtube'
 }
-//importing font from google
 const inter = Inter({ subsets: ["latin"]})
 
-export default function RootLayout({ children }: 
-    { children: React.ReactNode }) {
-    return (<ClerkProvider>
-        <html lang="en">
-            <body className={`$inter.className bg-dark-1`}>
-                {children}
-            </body>
-        </html>
-    </ClerkProvider>)
+export default function RootLayout({ 
+    children, 
+}: { 
+    children: React.ReactNode 
+}) {
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
+    )
 }
