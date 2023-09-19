9bf1873440f83501851c0f80b7bf957ed2432fa4
Evan <116227653+itsevanb@users.noreply.github.com>
2023-09-18 23:55:50 -0500

    setting up homepage layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,15 @@
-
 import '../globals.css'
 import { ClerkProvider } from "@clerk/nextjs"
 import { Inter } from "next/font/google"
+
+
+import Topbar from '@/components/shared/Topbar'
+import LeftSidebar from '@/components/shared/LeftSidebar'
+import RightSidebar from '@/components/shared/RightSidebar'
+import Bottombar from '@/components/shared/Bottombar'
+
+
+
 
 export const metadata = {
     title: 'social-media',
@@ -18,7 +26,19 @@ export default function RootLayout({
         <ClerkProvider>
             <html lang="en">
                 <body className={`${inter.className} bg-dark-1`}>
-                    {children}
+                    <Topbar />
+                    <main>
+                        <LeftSidebar />
+
+                        <section className="main-container">
+                            <div className="w-full max-w-4xl">
+                                {children}
+                            </div>
+                        </section>
+
+                        <RightSidebar />
+                    </main>
+                    <Bottombar />
                 </body>
             </html>
         </ClerkProvider>
