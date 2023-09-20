eed1c25a6b20c4a33b46cebfa8926290c0a22292
Evan <116227653+itsevanb@users.noreply.github.com>
2023-09-20 00:04:08 -0500

    users can now post content
    adjusted sidebar colors, made them a bit darker

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -14,7 +14,7 @@ async function Page() {
 
     return (
         <>
-        <h1 className="head-text">Posting...</h1>
+        <h1 className="head-text">Post Content</h1>
 
         <PostThread userId={userInfo._id} />
         </>
